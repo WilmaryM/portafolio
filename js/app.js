42f216a10295accbb323df9f2eb4f3474e0c27fc
Wilmary <wilmarymercedes1404@gmail.com>
2024-09-11 17:05:05 -0400

    subiendo a github pages

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function responsiveMenu(){
-    let x = document.getElementById("nav");
-    if(x.className === ""){
-        x.className = "responsive";
-    }else{
-        x.className = "";
-    }
-}document.getElementById("icono-nav").addEventListener("click", responsiveMenu);
